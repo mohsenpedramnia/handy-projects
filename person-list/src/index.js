7a23ad2f024a8f55a9876302216ba5d3b625a78b
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-14 22:58:02 +0330

    Accessing variable in src img

--- person-list/src/index.js
+++ person-list/src/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Person = ({ img, name, job, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <article>
+      <img src={url} alt="person" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+    </article>
+  );
+};
+
 const PersonList = () => {
-  return <section>my content will be here</section>;
+  return (
+    <section>
+      <Person img="34" name="john" job="developer" />
+      <Person img="22" name="bob" job="designer" />
+      <Person img="45" name="david" job="The boss" />
+    </section>
+  );
 };
 
 ReactDOM.render(<PersonList />, document.getElementById("root"));
