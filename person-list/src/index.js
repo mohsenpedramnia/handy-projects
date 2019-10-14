9bdbe6f6ec63337f51868a2d0ad460dfcb47fd9a
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-14 22:36:59 +0330

    Initial setup

---
+++ person-list/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const PersonList = () => {
+  return <section>my content will be here</section>;
+};
+
+ReactDOM.render(<PersonList />, document.getElementById("root"));
