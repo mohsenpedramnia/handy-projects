9e0abdf4066ea9d1bbfa64a060a7e5389cd52681
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-14 23:27:26 +0330

    Add some style

--- person-list/src/index.js
+++ person-list/src/index.js
@@ -1,22 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "./index.css";
 const Person = ({ img, name, job, children }) => {
   const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
-    <article>
+    <article className="person">
       <img src={url} alt="person" />
       <h4>{name}</h4>
       <h4>{job}</h4>
+      {children}
     </article>
   );
 };
 
 const PersonList = () => {
   return (
-    <section>
+    <section className="person-list">
       <Person img="34" name="john" job="developer" />
-      <Person img="22" name="bob" job="designer" />
+      <Person img="22" name="bob" job="designer">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+          ratione?
+        </p>
+      </Person>
       <Person img="45" name="david" job="The boss" />
     </section>
   );
